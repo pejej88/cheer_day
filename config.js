abaffe73b806fdeb7291af530d2cc175512c5a5d
pejej88 <pejej88@naver.com>
2025-07-30 15:19:21 +0900

    Updates

--- config.js
+++ config.js
@@ -7,7 +7,7 @@ window.GEMINI_API_KEY = process.env.GEMINI_API_KEY || '';
 // 카카오 개발자 콘솔에서 가져온 설정
 window.KAKAO_CONFIG = {
   CLIENT_ID: 'd1e72c968b6e8b460896927f9ef87753', // JavaScript 키
-  REDIRECT_URI: 'https://pejej88.github.io/cheer_day/#/auth/kakao/callback', // 프로덕션 환경
+  REDIRECT_URI: 'https://pejej88.github.io/cheer_day/', // 프로덕션 환경 (카카오 개발자센터 설정과 일치)
   // REDIRECT_URI: 'http://localhost:5173/#/auth/kakao/callback', // 개발 환경
 };
 
