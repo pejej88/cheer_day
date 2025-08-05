1394911a01b187fdcd2b823623945fbc34bd726e
pejej88 <pejej88@naver.com>
2025-08-05 16:02:46 +0900

    test

--- main.tsx
+++ main.tsx
@@ -2,10 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
-if (window.Kakao && !window.Kakao.isInitialized()) {
-  window.Kakao.init(import.meta.env.VITE_KAKAO_JS_KEY);
-}
-
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
